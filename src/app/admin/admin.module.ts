078322b1992c13025f1d22d302b52daeed255d21
kendrick chukwuka <kendrickchibueze@gmail.com>
2024-01-29 22:44:42 +0100

    working on the category component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,16 +4,27 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import { PostCategoryComponent } from './components/post-category/post-category.component';
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    DashboardComponent
+    DashboardComponent,
+    PostCategoryComponent
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class AdminModule { }
